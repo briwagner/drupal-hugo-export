3c0fc150c607b02a960fe1100a422da57b6a327e
Brian Wagner <briwagner@outlook.com>
2020-02-28 15:12:51 -0600

    modify menu loader to support child items when exporting content from menu and formatting menu config file

--- src/HugoMenuLoader.php
+++ src/HugoMenuLoader.php
@@ -39,26 +39,41 @@ class HugoMenuLoader {
     $service = \Drupal::service('menu.link_tree');
     $tree = $service->load($menuName, new MenuTreeParameters());
     if ($tree) {
-      foreach ($tree as $item) {
-        /** @var Drupal\menu_link_content\Entity\MenuLinkContent $linkContent */
-        $linkContent = $item->link;
-        // Skip if link has absolute path.
-        if ($linkContent->getUrlObject()->isExternal()) {
-          continue;
-        }
-        // Skip if link is disabled in menu.
-        if (!$linkContent->isEnabled()) {
-          continue;
-        }
-        // TODO: what other type of entities could be here?
-        // How to handle <front>?
-        if (!empty($linkContent->getPluginDefinition()['metadata'])) {
-          $entities['nodes'][] = $linkContent->getPluginDefinition()['route_parameters']['node'];
+      $entities = $this->getNodesFromMenuTree($entities, $tree);
+    }
+    return $entities;
+  }
+
+  /**
+   * Get entities from menu tree.
+   *
+   * @param array $entities
+   *   List of entity IDs.
+   * @param \Drupal\Core\Menu\MenuLinkTreeElement[] $tree
+   *   Menu tree elements.
+   */
+  protected function getNodesFromMenuTree(&$entities, $tree) {
+    foreach ($tree as $item) {
+      /** @var Drupal\menu_link_content\Entity\MenuLinkContent $linkContent */
+      $linkContent = $item->link;
+      // Skip if link has absolute path.
+      if ($linkContent->getUrlObject()->isExternal()) {
+        continue;
+      }
+      // Skip if link is disabled in menu.
+      if (!$linkContent->isEnabled()) {
+        continue;
+      }
+      // TODO: what other type of entities could be here?
+      // How to handle <front>?
+      if (!empty($linkContent->getPluginDefinition()['metadata'])) {
+        $entities['nodes'][] = $linkContent->getPluginDefinition()['route_parameters']['node'];
+
+        // Check for child items.
+        if ($item->hasChildren) {
+          $this->getNodesFromMenuTree($entities, $item->subtree);
         }
       }
-    }
-    else {
-      // Failed to load the menu.
     }
     return $entities;
   }
@@ -66,66 +81,105 @@ class HugoMenuLoader {
   /**
    * Export menu to Hugo config.
    *
-   * @param string $menu
+   * @param string $menu_name
    *   Menu name.
    */
-  public function exportMenu(string $menu) {
+  public function exportMenu(string $menu_name) {
     $dirBase = "hugo_export";
     $dir = sprintf("public://%s/config/menus/", $dirBase);
     file_prepare_directory($dir, FILE_CREATE_DIRECTORY);
 
-    $items = $this->loadMenu($menu);
+    $items = $this->loadMenu($menu_name);
     $menuItems = [];
 
-    $service = \Drupal::service('menu.link_tree');
-    $tree = $service->load($menu, new MenuTreeParameters());
+    $treeService = \Drupal::service('menu.link_tree');
+    $tree = $treeService->load($menu_name, new MenuTreeParameters());
     if ($tree) {
-      foreach ($tree as $item) {
-        /** @var Drupal\menu_link_content\Entity\MenuLinkContent $linkContent */
-        $linkContent = $item->link;
-
-        if ($linkContent->getUrlObject()->isExternal()) {
-          continue;
-        }
-        if (!$linkContent->isEnabled()) {
-          continue;
-        }
-        // Get node ID.
-        if (!empty($linkContent->getPluginDefinition()['metadata'])) {
-          // Get node values from link item.
-          $nid = $linkContent->getPluginDefinition()['route_parameters']['node'];
-          $node = \Drupal::service('entity_type.manager')->getStorage('node')
-            ->load($nid);
-
-          if ($node && $node->isPublished()) {
-            $menuItems[] = [
-              'name' => $linkContent->getTitle(),
-              'weight' => intval($linkContent->getWeight()),
-              'url' => $node->toUrl()->toString(),
-            ];
-          }
-        }
-      }
+      $menuItems = $this->getLinksFromTree($menuItems, $tree);
     }
 
     // Export menu, using name from Drupal.
     // TODO: we need to make this camelcase it seems. No hyphens allowed.
-    $menuName = str_replace("-", "", $menu);
+    $menuName = str_replace("-", "", $menu_name);
+    // Build menu structure.
     $m = [$menuName => $menuItems];
+    // Convert to yaml format for Hugo.
     $yaml = Yaml::dump($m, 6, 2);
     $filename = $dir . "/menus.yaml";
     file_unmanaged_save_data($yaml, $filename, FILE_EXISTS_REPLACE);
   }
 
+  /**
+   * Process current menu item and any child elements
+   *
+   * @param array $menuItems
+   *   Array of menu items for Hugo export.
+   * @param \Drupal\Core\Menu\MenuLinkTreeElement[] $tree
+   *   Menu tree elements.
+   * @param string $parent
+   *   Parent menu item identifier.
+   *
+   * @return array || bool
+   *   Return menu item.
+   */
+  protected function getLinksFromTree(&$menuItems, $tree, $parent = '') {
+    foreach ($tree as $item) {
+      /** @var Drupal\menu_link_content\Entity\MenuLinkContent $linkContent */
+      $linkContent = $item->link;
+
+      // Skip if link is external.
+      if ($linkContent->getUrlObject()->isExternal()) {
+        continue;
+      }
+      // Skip if link is disabled.
+      if (!$linkContent->isEnabled()) {
+        continue;
+      }
+      // Get node ID.
+      if (!empty($linkContent->getPluginDefinition()['metadata'])) {
+        // Get node values from link item.
+        $nid = $linkContent->getPluginDefinition()['route_parameters']['node'];
+        $node = \Drupal::service('entity_type.manager')->getStorage('node')
+          ->load($nid);
+
+        if ($node && $node->isPublished()) {
+          // Geneate new item and add to menu list.
+          $newItem = $this->buildMenuItem($item, $node->toUrl()->toString(), $parent);
+          $menuItems[] = $newItem;
+          // Get subtree, if found, passing parent link.
+          if ($item->hasChildren) {
+            $this->getLinksFromTree($menuItems, $item->subtree, $newItem['identifier']);
+          }
+        }
+      }
+    }
+    return $menuItems;
+  }
+
+  /**
+   * Build menu item for Hugo menu.
+   *
+   * @param \Drupal\Core\Menu\MenuLinkTreeElement $item
+   *   Menu item.
+   * @param string $url
+   *   Url for menu item.
+   * @param string $parent
+   *   Parent menu identifier.
+   *
+   * @return array
+   *   Array of menu item and children.
+   */
+  protected function buildMenuItem($item, $url, $parent = '') {
+    $menuItem = [
+      'name' => $item->link->getTitle(),
+      'weight' => intval($item->link->getWeight()),
+      'url' => $url,
+      'identifier' => strtolower($item->link->getTitle()),
+    ];
+    if ($parent != '') {
+      $menuItem['parent'] = $parent;
+    }
+    return $menuItem;
+  }
+
 }
-
-// https://gohugo.io/content-management/menus/#nesting
-// how to deal with nesting in hugo menu
-// that's the identifier: ...
-
-/**
- * Todo:
- *
- * - handle nested pages
- * - what to do with weights? are negative weights ok? YES!
- */
